8bfd064a6bbe8c4f1bf25e08ab441124c611a373
Septian Adi Wijaya <developadi@gmail.com>
2022-02-12 22:54:40 +0700

    setup - folder structured & more

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCards';
 
-function App() {
+const App = () => {
+
+  const beginQuiz = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        Quiz
+        <h1>QUIZ</h1>
+
+        <button className="begin" onClick={beginQuiz}>Begin</button>
+        <p className="score">Score:</p>
+        <p>Loading Question...</p>
+
+        <QuestionCard />
+
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
       </header>
     </div>
   );
