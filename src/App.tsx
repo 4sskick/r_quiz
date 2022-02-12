2dace36f14defc0b77ff83bf965c6fef3de017bf
Septian Adi Wijaya <developadi@gmail.com>
2022-02-12 22:11:58 +0700

    fix some errors & lib styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Quiz
+      <header className="App-header">
+        Quiz
+      </header>
     </div>
   );
 }
